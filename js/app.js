4e12501c51e200e4373b479048ff3091613a5a14
marcuspereiragithub <vinnycoast@gmail.com>
2018-03-26 16:37:51 -0300

    Adding anime scroll and debounce

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+// DEBOUNCE DO LODASH
+
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+//MUDAR TAB AO CLIQUE
+
 $('[data-group]').each(function(){
 	var $allTarget = $(this).find('[data-target]'),
 			$allClick = $(this).find('[data-click]'),
@@ -30,6 +49,8 @@ $('.menu-nav a[href^="#"]').click(function(e){
 	}, 500);
 });
 
+//SCROLL SUAVE PARA O TOPO
+
 $('.logo').click(function(e){
 	e.preventDefault();
 	$('html, body').animate({
@@ -37,6 +58,7 @@ $('.logo').click(function(e){
 	}, 500)
 });
 
+// MUDAR PARA ACTIVE O MENU DE ACORDO COM A ÁREA
 $('section').each(function(){
 	var height = $(this).height(),
 			offsetTop = $(this).offset().top,
@@ -44,54 +66,78 @@ $('section').each(function(){
 			id = $(this).attr('id'),
 			$itemMenu = $('a[href="#' + id + '"]');
 	
-	$(window).scroll(function(){
+	$(window).scroll(debounce(function(){
 		var scrollTop = $(window).scrollTop();
 		if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
 			$itemMenu.addClass('active');
 		} else {
 			$itemMenu.removeClass('active');
 		}
-	});
+	}, 200));
 });
 
+// BOTÃO DO MENU MOBILE
 
 $('.mobile-btn').click(function(){
 	$(this).toggleClass('active');
 	$('.mobile-menu').toggleClass('active');
 });
 
+// SLIDER
+(function(){
+    function slider(sliderName, velocidade) {
+        var slideClass = '.' + sliderName,
+            activeClass = 'active',
+            rotate = setInterval(rotateSlide, velocidade);
 
-function slider(sliderName, velocidade) {
-    var slideClass = '.' + sliderName,
-        activeClass = 'active',
-        rotate = setInterval(rotateSlide, velocidade);
-    
-    $(slideClass + ' > :first').addClass(activeClass);
-    
-    $(slideClass).hover(function(){
-        clearInterval(rotate);
-    }, function(){
-        rotate = setInterval(rotateSlide, velocidade);
-    });
+        $(slideClass + ' > :first').addClass(activeClass);
 
-    function rotateSlide() {
-        var activeSlide = $(slideClass + ' > .' + activeClass),
-            nextSlide = activeSlide.next();
+        $(slideClass).hover(function(){
+            clearInterval(rotate);
+        }, function(){
+            rotate = setInterval(rotateSlide, velocidade);
+        });
 
-        if(nextSlide.length == 0) {
-            nextSlide = $(slideClass + ' > :first');
+        function rotateSlide() {
+            var activeSlide = $(slideClass + ' > .' + activeClass),
+                nextSlide = activeSlide.next();
+
+            if(nextSlide.length == 0) {
+                nextSlide = $(slideClass + ' > :first');
+            }
+            activeSlide.removeClass(activeClass);
+            nextSlide.addClass(activeClass);
         }
-        activeSlide.removeClass(activeClass);
-        nextSlide.addClass(activeClass);
+
     }
-    
-}
-slider('introducao', 3000);
+    slider('introducao', 3000);
+})();
 
+// ANIMAÇÃO AO SCROLL
+(function(){
+    var $target = $('[data-anime="scroll"]'),
+        animationClass = 'animate',
+        offset = $(window).height() * 3/4;
 
+    function animeScroll() {
+        var documentTop = $(window).scrollTop();
 
+        $target.each(function(){
+            var itemTop = $(this).offset().top;
+            if (documentTop > itemTop - offset) {
+                $(this).addClass(animationClass);
+            }else {
+                $(this).removeClass(animationClass);
+            }
+        });
+    }
 
+    animeScroll();
 
+    $(document).scroll(debounce(function(){
+        animeScroll();
+    }, 200));
+})();
 
 
 
